a2aed6b30ceda45a5585b75fa148e23790f9fd5a
arshaansayed <arshaan.h.sayed@gmail.com>
2022-06-25 11:29:09 -0400

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Arshaan Sayed on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Arshaan Sayed on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
